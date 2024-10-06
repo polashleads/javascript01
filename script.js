b6e6e8157cd2ab10bcf1b39e869efe7ac1fef90e
polashleads <joita8877@gmail.com>
2024-10-06 19:27:48 +0600

    ad a css file

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-function displayDate(){
-    document.getElementById('demo').innerHTML=Date();
-}
+let msg
+msg= `"<br>we use getElement for display the<br>'content'<br>"`;
+
+document.getElementById('demo').innerHTML= msg;
 
